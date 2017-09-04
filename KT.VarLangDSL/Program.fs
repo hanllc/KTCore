4a71d0768e49c211d3cbebb1efeffad2a651034a
jman <hanllc@gmail.com>
2017-09-04 14:11:19 -0400

    Initial

---
+++ KT.VarLangDSL/Program.fs
@@ -0,0 +1,88 @@
+﻿
+// Spike DSL on CLR for Market Extract 
+// Given a declarative statement of extract,
+// produce one or more SQL Statements
+
+open System
+open System.Collections.Generic
+open FSharp.Collections
+
+type ExColumn() =
+    [<DefaultValue>] val mutable Name : string
+    [<DefaultValue>] val mutable ExColumnName : string
+    [<DefaultValue>] val mutable DbObjectName : string
+
+type ExTable() = 
+    [<DefaultValue>] val mutable TableId : string
+    let mutable exCols = new Dictionary<string, ExColumn>()
+    member this.AddCol (col:ExColumn) =
+                let key = col.ExColumnName
+                exCols.Add(key, col)
+
+type ExModel() = 
+    [<DefaultValue>] val mutable ModelId : string
+    let mutable exColumnList : ExColumn list = []
+    member this.ExColumnList with get() = exColumnList
+  
+    let mutable ExTableDict = new Dictionary<string, ExTable>()
+    member this.AddCol (col:ExColumn) =
+            exColumnList <- col :: exColumnList
+            let xtab =
+                if ExTableDict.ContainsKey(col.DbObjectName) = true then
+                    //printfn "HasTable %s" col.DbObjectName
+                    ExTableDict.[col.DbObjectName]
+                else
+                    //printfn "DoesNotHaveTable %s" col.DbObjectName
+                    let tab = new ExTable()
+                    tab.TableId <- col.DbObjectName
+                    ExTableDict.Add( col.DbObjectName, tab)
+                    tab 
+            xtab.AddCol col
+
+let dsl_extract = new ExModel()
+
+let save name = 
+    dsl_extract.ModelId <- name
+
+let rec show_cols (colList:ExColumn list) =
+    match colList with
+        | head :: tail -> show_cols tail; printfn "%s" head.ExColumnName
+        | [] -> printfn ":"
+
+
+let show label = 
+    printfn "%s" dsl_extract.ModelId
+    show_cols dsl_extract.ExColumnList
+
+
+
+let source = 1 
+
+let variable nameStr sourceKey tabStr = 
+    let me = new ExColumn()
+    me.Name <- nameStr
+    me.ExColumnName <- nameStr
+    me.DbObjectName <- tabStr
+    dsl_extract.AddCol me
+    ()
+
+
+[<EntryPoint>]
+let main argv =
+    //EXAMPLE DSL
+
+    variable "var1.1" source "pardat_sale"
+    variable "var1.2" source "pardat_sale"
+
+    variable "var2.1" source "land_sale"
+    variable "var2.2" source "land_sale"
+
+    variable "var3.1" source "dweldat_sale"
+    variable "var3.2" source "dweldat_sale"
+
+
+    save "myextract"
+
+    show "label"
+    
+    0 // return an integer exit code
---
+++ README.txt
@@ -0,0 +1,51 @@
+
+Spike motivated by DSL articles 
+https://www.codeproject.com/Articles/39031/Making-DSLs-in-F
+
+https://github.com/dungpa/dsls-in-action-fsharp/blob/master/DSLCheatsheet.md
+http://dungpa.github.io/fsharp-cheatsheet/
+
+http://imps.mcmaster.ca/pouyalarjani/fscodegen.pdf
+https://blog.inf.ed.ac.uk/aplcourse/2009/02/heterogeneous-metaprogramming-fsharp/
+
+
+wow - agent based CQRS
+http://apprize.info/programming/f_1/9.html
+
+http://apprize.info/programming/f_1/index.html
+http://funscript.info/
+
+http://www.fssnip.net/qL/title/Dict-utilities-to-use-Dictionary-in-more-Fy-way
+
+Nice example code
+https://github.com/stuartjdavies/FSharp.Cloud.AWS/blob/master/FSharp.Cloud.AWS/DynamoDB.fs
+
+RUST ORM
+http://diesel.rs/guides/getting-started/
+
+SCALA DSL
+http://debasishg.blogspot.com/2008/04/external-dsls-made-easy-with-scala.html
+https://github.com/deanwampler/SeductionsOfScalaTutorial/blob/master/tutorial-exercises/ex11-external-dsl.scala
+https://hedleyproctor.com/2014/11/writing-a-dsl-with-scala-parser-combinators/
+https://www.infoq.com/articles/External-DSL-Vaughn-Vernon
+
+NICE ARTICLE
+http://www.havelund.com/Publications/dsl-scala-2015.pdf
+
+
+variable "var1.1" source "pardat_sale"
+
+use var1 var2 var3 from table1
+use varx from table2
+exlude var3 var5 var6 from table2
+use all from table3
+
+over table7 name main_baths is main_bath_count card is 1
+
+over table4 name total_rooms is sum of sfla
+
+over table5 name total_stories is max of stories 
+
+over this name sales_by_month_year as count unique salekey by month and year
+
+name lot_to_improved as total_rooms / total_stories
